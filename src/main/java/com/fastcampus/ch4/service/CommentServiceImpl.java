f0633b181807ce1cc6c227e41aed62539997fcb4
hunjeong93 <rhkrgnswjd@naver.com>
2022-11-11 17:01:06 +0900

    comment DB,Mapper,DAO,Service UPDATE

---
+++ src/main/java/com/fastcampus/ch4/service/CommentServiceImpl.java
@@ -0,0 +1,66 @@
+package com.fastcampus.ch4.service;
+
+
+
+import com.fastcampus.ch4.dao.BoardDao;
+import com.fastcampus.ch4.dao.CommentDao;
+import com.fastcampus.ch4.domain.CommentDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.*;
+
+@Service
+public class CommentServiceImpl implements CommentService {
+//    @Autowired
+    BoardDao boardDao;
+//    @Autowired
+    CommentDao commentDao;
+
+    @Autowired
+    public CommentServiceImpl(CommentDao commentDao, BoardDao boardDao) {
+        this.commentDao = commentDao;
+        this.boardDao = boardDao;
+    } // 생성자 주입. 생성자 하나만 있을 때 가능. 의존성 주입. 매개변수 하나만 들어와서 생성되는 실수 방지
+
+    @Override
+    public int getCount(Integer bno) throws Exception {
+        return commentDao.count(bno);
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public int remove(Integer cno, Integer bno, String commenter) throws Exception {
+        int rowCnt = boardDao.updateCommentCnt(bno, -1);
+        System.out.println("updateCommentCnt - rowCnt = " + rowCnt);
+//        throw new Exception("test");
+        rowCnt = commentDao.delete(cno, commenter);
+        System.out.println("rowCnt = " + rowCnt);
+        return rowCnt;
+    }
+
+    @Override
+    @Transactional(rollbackFor = Exception.class)
+    public int write(CommentDto commentDto) throws Exception {
+        boardDao.updateCommentCnt(commentDto.getBno(), 1);
+//                throw new Exception("test");
+        return commentDao.insert(commentDto);
+    }
+
+    @Override
+    public List<CommentDto> getList(Integer bno) throws Exception {
+//        throw new Exception("test");
+        return commentDao.selectAll(bno);
+    }
+
+    @Override
+    public CommentDto read(Integer cno) throws Exception {
+        return commentDao.select(cno);
+    }
+
+    @Override
+    public int modify(CommentDto commentDto) throws Exception {
+        return commentDao.update(commentDto);
+    }
+}
